2f3908c4826e510f0e5f8fc6e55c78af878794f3
Shilpa Mathew <Shilpa.Mathew@serko.com>
2022-11-06 17:07:56 +1300

    Init repo, use inmemory

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookHistory.Models
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string? Author { get; set; }
+    }
+}
---
+++ Models/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BookHistory.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; } = null!;
+    }
+}
